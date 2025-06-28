f565254194b7e14e33ed8f276b975f9085c56cad
udud0099 <udud0099@gmail.com>
2025-06-28 11:20:05 +0545

    Project-13 -Math Game - finish

---
+++ Project-39/project/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-black">
+        <h1 className="text-white font-bold text-8xl">Just do it - A A</h1>
+      </div>
+    </>
+  );
+}
